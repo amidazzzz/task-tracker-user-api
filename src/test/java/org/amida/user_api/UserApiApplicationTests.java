c84c3b0ed5dbe766eb7fbeff5da89fa26ff8c2f0
amida <cain.wells@mail.ru>
2024-10-08 18:39:57 +0300

    Initial commit

---
+++ src/test/java/org/amida/user_api/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.amida.user_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
