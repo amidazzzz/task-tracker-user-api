df5dd5de88c34af3d522c44230b247bc63ecd242
amida <cain.wells@mail.ru>
2024-10-08 19:47:50 +0300

    feat: added Task model class

--- src/main/java/org/amida/user_api/model/Task.java
+++ src/main/java/org/amida/user_api/model/Task.java
@@ -1,4 +1,31 @@
 package org.amida.user_api.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(schema = "public", name = "t_task")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", referencedColumnName = "id", nullable = false)
+    private User owner;
+
+    @Column(name = "timestamp")
+    private LocalDateTime timestamp;
+
 }
