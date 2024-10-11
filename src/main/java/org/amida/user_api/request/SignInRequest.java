9a2386ac515113d61368c31c7530cdf116788d42
amida <cain.wells@mail.ru>
2024-10-11 17:29:21 +0300

    feat: added SignIn request dto

--- src/main/java/org/amida/user_api/request/SignInRequest.java
+++ src/main/java/org/amida/user_api/request/SignInRequest.java
@@ -1,4 +1,14 @@
 package org.amida.user_api.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class SignInRequest {
+
+    @NotBlank(message = "")
+    private String username;
+
+    @NotBlank(message = "")
+    private String password;
 }
