22451999356db5de20a54b5b2b0cf9750744cc64
amida <cain.wells@mail.ru>
2024-10-11 22:09:45 +0300

    feat: configured UserDetailsSeriveImpl class

--- src/main/java/org/amida/user_api/config/UserDetailsServiceImpl.java
+++ src/main/java/org/amida/user_api/config/UserDetailsServiceImpl.java
@@ -1,4 +1,25 @@
 package org.amida.user_api.config;
 
-public class UserDetailsServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.amida.user_api.model.User;
+import org.amida.user_api.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
 }
