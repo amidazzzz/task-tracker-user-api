3963f59f80d530e1e3eea324afee8457e1280d39
amida <cain.wells@mail.ru>
2024-10-11 22:11:27 +0300

    feat: configured rest controller AuthController class

--- src/main/java/org/amida/user_api/controller/AuthController.java
+++ src/main/java/org/amida/user_api/controller/AuthController.java
@@ -1,4 +1,26 @@
 package org.amida.user_api.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.amida.user_api.request.SignUpRequest;
+import org.amida.user_api.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    @GetMapping("/register")
+    public ResponseEntity<String> register(@RequestBody SignUpRequest request){
+        authService.register(request);
+        return ResponseEntity.ok("The user registered successfully");
+    }
+
+
 }
