8aff906c92cc0f8090ca13adbb8af75304dcfbd3
amida <cain.wells@mail.ru>
2024-10-12 13:40:27 +0300

    feat: added login method in AuthController

--- src/main/java/org/amida/user_api/controller/AuthController.java
+++ src/main/java/org/amida/user_api/controller/AuthController.java
@@ -1,26 +1,33 @@
 package org.amida.user_api.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.amida.user_api.request.SignInRequest;
 import org.amida.user_api.request.SignUpRequest;
 import org.amida.user_api.service.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/auth")
+@RequestMapping("api/v1/auth")
 public class AuthController {
 
     private final AuthService authService;
 
-    @GetMapping("/register")
+    @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody SignUpRequest request){
         authService.register(request);
         return ResponseEntity.ok("The user registered successfully");
-    }
+    } // непонятны сообщения статуса исполнения
+    // не работают constraints
 
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody SignInRequest request){
+        authService.authenticate(request);
+        return ResponseEntity.ok("The user logged in successfully");
+    }
 
 }
